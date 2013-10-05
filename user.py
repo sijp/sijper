2415ce4e4f4bf7e96a7ae20786faec3d7ac30c7f
sijp <sijproject@gmail.com>
2013-10-05 18:06:07 +0300

    user-usermanager idea

--- user.py
+++ user.py
@@ -1,18 +1,34 @@
+# Basic User class
+# Contains a User ID (uid) and a list of references to other User objects
+# which correspond to the users this user if following
+
 class User:
+
+	#Users this user object is following
+	#initialised to empty list
 	following=[]
 	
-	def __init__(self):
-		pass
+	#constructor - recieves a uid corresponding to this user
+	def __init__(self,uid):
+		self.uid=uid
+
+	#follow a user u
+	#u - a reference to another User object
 	
 	def follow(self,u):
 		self.following.append(u)
 	
+	#unfollow a user u
+	#u - a reference to another User object
+	#returns True if this user followed `u` before,
+	#	 otherwise False
 	def unfollow(self,u):
 		try:
 			self.following.remove(u)
 			return True
 		except ValueError:
 			return False
-	
+
+	#returns a list of the users this user if following
 	def getFollowingList(self):
 		return self.following
