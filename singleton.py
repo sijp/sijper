9abe4579e9835502cdec3a5e859863c3347863d0
sijp <sijproject@gmail.com>
2013-10-06 22:53:42 +0300

    Removing oldstyle singleton-model files

--- singleton.py
+++
@@ -1,8 +0,0 @@
-class Singleton(type):
-	ins={}
-
-	def __call__(cls,*args,**kwargs):
-		print cls.ins
-		if cls not in cls.ins:
-			cls.ins[cls]=super(Singleton,cls).__call__(*args,**kwargs)
-		return cls.ins[cls]
